14e0b2362c06508fc7b91d85c278f02c0e36c1c1
petrleba <131344580+petrleba@users.noreply.github.com>
2025-10-02 09:30:57 +0200

    Add files via upload
    Přidání větvení programu

---
+++ vetveni-programu/js/script.js
@@ -0,0 +1,10 @@
+let pocet = 20;
+const pi = 3.14;
+
+if (pocet > 30) {
+  alert("Počet je větší než 30");
+} else if (pocet < 10) {
+  alert("Počet je menší než 10");
+} else {
+  alert("Počet je větší než 10 a menší než 30");
+}
